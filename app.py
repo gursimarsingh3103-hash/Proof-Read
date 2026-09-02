ed41840ba652ed11b0319a0c64add582982ad16b
gursimarsingh3103-hash <gursimarsingh3103@gmail.com>
2026-09-02 15:46:04 +1200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if st.button("Proofread & Improve"):
                 """
                 
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-3.6-flash",
                     contents=prompt
                 )
                 
